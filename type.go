54e5611f70a9fa10d937b00058eff13c51861ca2
Mahmoud Eskandari <mahmoud-eskandari@users.noreply.github.com>
2023-12-30 16:59:58 +0330

    Update and rename goje.go to type.go

---
+++ type.go
@@ -0,0 +1,28 @@
+package goje
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+)
+
+// Context and Database Handler struct
+type ContextHandler struct {
+	DB  *sql.DB
+	Ctx context.Context
+}
+
+// Entity All generated structs implemented from Entity
+type Entity interface {
+	TableName() string
+	Columns() []string
+	GetCtx() *ContextHandler
+	SetCtx(*ContextHandler)
+	GetParent() *Entity
+}
+
+var (
+	ErrHandlerIsNil       = errors.New("context handler dosen't set properly")
+	ErrRecursiveLoad      = errors.New("recursive load is forbidden")
+	ErrNoColsSetForUpdate = errors.New("cols should have at least one proprty for update")
+)
